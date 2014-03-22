4604d18062c7f44b89fc0680b82a5f8806d1529e
Chris Dickinson <christopher.s.dickinson@gmail.com>
2014-03-22 16:25:18 -0700

    modify moratorium on nested objects

--- checks/warn-nested-objects.js
+++ checks/warn-nested-objects.js
@@ -3,9 +3,12 @@ module.exports = nested_object
 nested_object.selector = 'object > * > object'
 
 function nested_object(node, subsource, alert) {
-  alert(
-      node
-    , 'nesting object literals inside of other object literals is discouraged.'
-  )
+  if(node.properties.length > 1) {
+    alert(
+        node
+      , 'nesting object literals inside of other ' +
+        'object literals is discouraged.'
+    )
+  }
 }
 
